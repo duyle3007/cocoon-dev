f8bf812c30529c0d1423ebad79da19135dc44beb
duyle3007 <duyle3007@gmail.com>
2023-03-26 11:29:07 +0700

    Install antd and locomotive-scroll

--- frontend/components/pages/HomePage/HomePage.js
+++ frontend/components/pages/HomePage/HomePage.js
@@ -5,20 +5,6 @@ const HomePage = () => {
     <div className="homepage">
       <Header />
       This is homepage
-      {/* <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyBMFpople89Rc8M0oXW0UTdeQbBr_cntkU" }}
-        defaultCenter={{
-          lat: 10.99835602,
-          lng: 77.01502627,
-        }}
-        defaultZoom={11}
-      >
-        <LocationPin
-          lat={37.42216}
-          lng={-122.08427}
-          text="1600 Amphitheatre Parkway, Mountain View, california."
-        />
-      </GoogleMapReact> */}
     </div>
   );
 };
