a3c32a8674949d7ab7b1a8181f2480a79277c5c4
duyle3007 <duyle3007@gmail.com>
2023-07-15 16:20:39 +0700

    Upgrade css scroll for search page

--- frontend/components/Layout/Layout.js
+++ frontend/components/Layout/Layout.js
@@ -1,7 +1,8 @@
 import Header from "@/components/Header/Header";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import { notification } from "antd";
+import { useRouter } from "next/router";
 
 import Footer from "../Footer/Footer";
 
@@ -10,24 +11,37 @@ import styles from "./Layout.module.scss";
 export const PropertyListContext = createContext();
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+
   const [propertyList, setPropertyList] = useState([]);
   const [mediaList, setMediaList] = useState([]);
+  const [allLocation, setAllLocation] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
+      const searchMotoPress = axios.get(
+        "https://cocoonluxury.in/wp-json/mphb/v1/accommodation_types",
+        {
+          auth: {
+            username: process.env.NEXT_PUBLIC_MOTOPRESS_USERNAME,
+            password: process.env.NEXT_PUBLIC_MOTOPRESS_PASSWORD,
+          },
+        }
+      );
+      const searchWp = axios.get(
+        "https://cocoonluxury.in/wp-json/wp/v2/mphb_room_type"
+      );
+
+      const getAllLocation = axios.get("/api/locations");
       try {
-        const { data: resMoto } = await axios.get(
-          "https://cocoonluxury.in/wp-json/mphb/v1/accommodation_types",
+        const [
+          { data: resWp },
+          { data: resMoto },
           {
-            auth: {
-              username: process.env.NEXT_PUBLIC_MOTOPRESS_USERNAME,
-              password: process.env.NEXT_PUBLIC_MOTOPRESS_PASSWORD,
-            },
-          }
-        );
-        const { data: resWp } = await axios.get(
-          "https://cocoonluxury.in/wp-json/wp/v2/mphb_room_type"
-        );
+            data: { data: allLocation },
+          },
+        ] = await Promise.all([searchWp, searchMotoPress, getAllLocation]);
+
         const res = resMoto.map((result) => {
           const findItemInWp = resWp.find(
             (otherRes) => otherRes.id === result.id
@@ -39,6 +53,7 @@ const Layout = ({ children }) => {
           }
         });
         setPropertyList(res);
+        setAllLocation(allLocation);
       } catch (err) {
         console.log("Fetch list data", err);
         notification.error({
@@ -61,17 +76,25 @@ const Layout = ({ children }) => {
     getMediaList();
   }, []);
 
+  const isShowFooter = useMemo(() => {
+    if (router.pathname !== "/search") {
+      return true;
+    }
+    return false;
+  }, [router]);
+
   return (
     <PropertyListContext.Provider
       value={{
         propertyList: propertyList,
         mediaList: mediaList,
+        allLocation: allLocation,
       }}
     >
       <div className={styles.layout}>
         <Header />
         <div className={styles.bodyWrapper}>{children}</div>
-        <Footer />
+        {isShowFooter && <Footer />}
       </div>
     </PropertyListContext.Provider>
   );
